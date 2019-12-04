64f227fe5c968857d5786735aff762ffe3425375
Ильсур Гильмутдинов <51150304+ilsur-dev@users.noreply.github.com>
2019-12-05 00:27:45 +0500

    1.2
    * Добавлено много пасхалок
    * Добавлены консольные команды - lms.help()
    * Переведено меню пользователя
    * Оптимизировано обновление элементов на странице

--- lms_script.meta.js
+++ lms_script.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Yandex Lyceum - Text Updater
-// @version      1.1
-// @description  Скрипт, который моментально меняет содержимое Яндекс LMS на кастомный текст
+// @name         Yandex Lyceum - Addon
+// @version      1.2
+// @description  Аддон для Яндекс LMS, который добавляет доп. функционал и кастомные тексты
 // @author       Ilsur Gilmutdinov - ilsur_dev
 // @match        *://lyceum.yandex.ru/*
 // @match        *://lms.yandex.ru/*
@@ -9,4 +9,5 @@
 // @updateUrl    https://github.com/ilsur-dev/yandex-lms-updater/raw/master/lms_script.meta.js
 // @run-at       document-start
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @grant        none
 // ==/UserScript==
