adcc23544edbcaa2a44ee9bb2abc26c2311fb7e1
Ильсур Гильмутдинов <51150304+ilsur-dev@users.noreply.github.com>
2020-01-13 11:04:36 +0500

    Main changes

--- lms_script.user.js
+++ lms_script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Yandex Lyceum - Addon
-// @version      1.2
+// @version      1.3
 // @description  Аддон для Яндекс LMS, который добавляет доп. функционал и кастомные тексты
 // @author       Ilsur Gilmutdinov - ilsur_dev
 // @match        *://lyceum.yandex.ru/*
@@ -24,15 +24,18 @@
         var header_notify_title = 'Повестки';
         var header_notify_placeholder = 'Сударь, у Вас пока нет Повесток!';
 
+        var menu_email = 'Срёт говном';
         var menu_profile = 'Досье';
         var menu_edit = 'Управление ГУЛАГОМ';
         var menu_exit = 'Эмигрировать';
 
         var course_name = 'Основы программирования на удаве';
+        var course_info_button = 'Посрать говном';
 
         var teacher_name = 'Мастер Йода';
         var teacher_info = 'Великий магистр ордена джедаев';
 
+        var task_deadline_passed = ' — Дедлайн просран';
         var task_test_failed = 'БАН!!!11!!1';
         var task_id = 'Талон #';
         var task_comments_failed = 'Говно, переделывай'
@@ -47,6 +50,13 @@
         var footer_chat = 'Бла-Бла';
         var footer_help_button = 'Обкашлять вопросики';
         var footer_copyright = '© 1970 — 2019 «Тындекс»';
+
+        var error_401_title = 'Вы исключены';
+        var error_401_info = 'Не расстраивайтесь! С такими успехами вы легко сможете устроиться в Mail Ru Group';
+        var error_403_title = 'Съемка видео запрещена';
+        var error_403_info = 'Камеру вырубай, мудила!';
+        var error_help_button = 'Сисадмин';
+        var error_help_info = 'Если у вас возникли проблемы с сайтом, призовите сисадмина';
     // Конец конфигурации
 
     var $ = window.jQuery;
@@ -63,14 +73,18 @@
         $('h1.notification-center__title.notification-center__title_theme_light').text(header_notify_title);
         $('p.notification-center__message').text(header_notify_placeholder);
 
+        $('span.user-account__subname').text(menu_email);
         $('a.control.menu__item.user2__menu-item.user2__menu-item_type_link.menu__item.menu__item_type_link:contains("Профиль")').text(menu_profile);
         $('a.control.menu__item.user2__menu-item.user2__menu-item_type_link.user2__menu-item_action_passport.menu__item.menu__item_type_link:contains("Управление аккаунтом")').text(menu_edit);
         $('a.control.menu__item.user2__menu-item.user2__menu-item_type_link.user2__menu-item_action_exit.menu__item.menu__item_type_link:contains("Выйти")').text(menu_exit);
 
         $('span.main-nav__course-name, main.layout__main > h1.heading.heading_level_1, h4.course-card__title:contains("Python"), h1.heading.heading_level_1:contains("Python"), a.student-profile-course-card__course-link').text(course_name);
+        $('p > strong > a:contains("Принять участие")').text(course_info_button);
 
-        $('div.teacher-info__content.user-info.user.user_size_l > div.user__info > a.user__name-link > span.user__name, div.student-profile-course-card__teacher.user.user_size_m > div.user__info > a.user__name-link > span.user__name').text(teacher_name);
+        $('span.user__name:contains("Павлов Сергей")').text(teacher_name);
         $('a.user-info__email').text(teacher_info);
+
+        $('span.lesson-card__deadline-message:contains("прошёл")').text(task_deadline_passed);
 
         $('h4.heading.heading_level_4.solution-status-viewer__status-text:contains("Доработать")').text(task_test_failed);
 
@@ -86,6 +100,14 @@
         $('.ya-chat-button__text-container').text(footer_chat);
         $('a.footer__help-link').text(footer_help_button + " (Матвей Волков - HTML программист)");
         $('.footer__copyright').text(footer_copyright);
+
+        $('h1.heading.heading_level_1.error-page__title:contains("не зарегистрированы")').text(error_401_title);
+        $('p.error-page__description:contains("ввести логин и пароль")').text(error_401_info);
+        $('h1.heading.heading_level_1.error-page__title:contains("нет прав доступа")').text(error_403_title);
+        $('p.error-page__description:contains("Если это не так")').text(error_403_info);
+
+        $('span.Button2-Text:contains("Обратная связь")').text(error_help_button);
+        $('p.error-page__feedback-text:contains("возникли проблемы")').text(error_help_info);
 
         try {
             $('a.comments__file-link.comments__file-link_active').html($('a.comments__file-link.comments__file-link_active').html().replace('ID&nbsp;', task_id));
@@ -108,6 +130,7 @@
         console.log("lms.help() - Показать справку по скрипту");
         console.log("lms.disableOptimization() - Отключить оптимизированное обновление страницы. Это увеличит скорость обновления текста на сайте")
         console.log("lms.titleEdit() - Меняет заголовок вкладки на текст-пасхалку)")
+        console.log("lms.createNotify() - Инициирует новое уведомление [Функция в разработке]")
         console.log("==========================================");
     };
     lms.disableOptimization = function() {
@@ -120,6 +143,9 @@
         page_title_modify = !page_title_modify;
         console.log("[LMS Addon]: Пасхалка включена. Введите lms.titleEdit() для отключения")
     };
+    lms.createNotify = function() {
+        $('div.notifications').append('<div class="notifications"><div class="notifications__item notifications__item_type_error notifications__item-enter-done"><div class="notifications__icon-wrapper"><span data-lego="react" class="icon icon_type_cross-red icon_size_m"></span></div>Проблемы с соединением<button type="button" class="Button2 Button2_size_m Button2_theme_clear Button2_view_lyceum notifications__remove-button" autocomplete="off"><span data-lego="react" class="icon icon_type_close-thin icon_size_m Button2-Icon"></span></button></div></div>');
+    };
 
     var eggLms = window.eggLms = {};
     eggLms.jaba = function () {
@@ -130,7 +156,7 @@
         if (i > 300) {
             console.log("[LMS Addon]: Интервал обновления страницы переключён в оптимальный режим");
             clearInterval(fastUpdater);
-            var slowUpdater = setInterval(updatePage, 20);
+            var slowUpdater = setInterval(updatePage, 200);
             clearInterval(timerOptimize);
         };
     }, 1000);
